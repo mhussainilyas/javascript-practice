276a1b20a56cebe1d014299c748c5a0c9fac65e2
mhussainilyas <mhussainilyas7@gmail.com>
2024-12-05 16:13:45 -1200

    math object

---
+++ math-object.js
@@ -0,0 +1,27 @@
+// !properties
+// let num = Math.PI;
+// let num = Math.LN2;
+// let num = Math.LN10;
+// let num = Math.SQRT2;
+// let num = Math.SQRT1_2;
+// let num = Math.LOG2E;
+// let num = Math.LOG10E;
+// console.log(num);
+
+// !Methods()
+// let num = Math.abs(-10);
+// let num = Math.round(98.6);
+// let num = Math.ceil(98.6);
+// let num = Math.floor(98.6);
+// let num = Math.trunc(-98.6);
+// let num = Math.sign(98.6);
+// let num = Math.sign(-87);
+// let num = Math.sqrt(64);
+// let num = Math.pow(2, 4);
+// let num = Math.log(2);
+// let num = Math.log2(4);
+// let num = Math.random();
+// let num = Math.random() * 10;
+// let num = Math.random() * 100;
+let num = Math.round(Math.random() * 100);
+console.log(num);
--- string.js
+++ string.js
@@ -92,3 +92,124 @@ console.log(str);
 // let extractedCharacter = str.at(8);
 // let extractedCharacter = str.at(-8);
 // console.log(extractedCharacter);
+
+// =====================================
+//     Some additional method 
+// =====================================
+
+// --------- toUpperCase() method --------
+// let upperLetter = str.toUpperCase();
+// console.log(upperLetter);
+
+// --------- toUpperCase() method --------
+// let lowerLetters = str.toLowerCase();
+// console.log(lowerLetters);
+
+// --------- trim() method --------
+// let string = "         !Muhammad Hussain         ";
+// console.log(string.length);
+// let newStr = string.trim();
+// console.log(newStr);
+// console.log(newStr.length);
+
+// --------- split() method --------
+// let newStr = str.split();
+// let newStr = str.split("");
+// let newStr = str.split(" ");
+// console.log(newStr);
+
+// --------- reverse() method --------
+// let newStr = str.split(" ").reverse();
+// console.log(newStr);
+
+// --------- join() method --------
+// let newStr = str.split(" ").reverse().join();
+// console.log(newStr);
+
+// =====================================
+//           Practice Example 
+// =====================================
+
+// !Example-01
+// console.log("a".charCodeAt());
+// console.log('z'.charCodeAt());
+// console.log('A'.charCodeAt());
+// console.log('Z'.charCodeAt());
+
+// for(let char = 97; char <= 122; char++) {
+//    console.log(char);
+//    console.log(String.fromCharCode(char));
+// }
+
+// for(let char = 65; char <= 90; char++) {
+//    console.log(String.fromCharCode(char));
+// }
+
+// !Example-02
+// const checkAllVowelPresentOrNot = (str) => {
+//    let vowel = "aeiou";
+//    for(let char of vowel) {
+//       console.log(str.includes(char));
+//       debugger;
+//       if(!str.includes(char)) {
+//          return false;
+//       }
+//    }
+//    return true;
+// }
+
+// checkAllVowelPresentOrNot(str);
+// console.log(checkAllVowelPresentOrNot("Muhammad Hussain"));
+// console.log(checkAllVowelPresentOrNot(str));
+
+// !Example-03
+// const countVowel = (str) => {
+//    let vowel = "aeiouAEIOU";
+//    let result = 0;
+//    for(let char of str) {
+//       if(vowel.includes(char)) {
+//          result++;
+//       }
+//    }
+//    return result;
+// }
+
+// console.log(countVowel(str));
+// console.log(countVowel("Hussain Ilyas"));
+
+// !Example-04
+// *The given string is pangram or not?
+
+// 👉👉 1st Method
+// const isPangram = (str) => {
+//    let totalAlpha = '';
+//    for(let char = 97; char <= 122; char++) {
+//       totalAlpha = totalAlpha + String.fromCharCode(char);
+//    }
+//    let string = str.toLowerCase();
+//    for(let char of totalAlpha) {
+//       if(!string.includes(char)) {
+//            return "Not Pangram String.";
+//       }
+//    }
+//    return "Pangram String.";
+// }
+
+// 👉👉 2nd Method
+const isPangram = (str) => {
+   let arrString = str.toLowerCase().split("");
+   const value = arrString.filter((item) => {
+      return item.charCodeAt(0) >= "a".charCodeAt(0) && item.charCodeAt(0) <= "z".charCodeAt(0)
+   })
+   // console.log(value);
+   // console.log([...new Set(value)]);
+   // console.log([...new Set(value)].length);
+   if([...new Set(value)].length === 26) {
+      return "Given String is pangram."
+   } else {
+      return "Given String is not pangram."
+   }
+}
+
+console.log(isPangram(str));
+// console.log(isPangram("Muhammad Hussain Ilyas"));
